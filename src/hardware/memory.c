5af149d10df1806dd5f0aa75573440a12446086b
= <>
2025-01-14 23:02:49 -0500

    continue

--- src/hardware/memory.c
+++ src/hardware/memory.c
@@ -1,3 +1,7 @@
 #include "stdint.h"
 #define MEMORY_MAX (1 << 16)
 uint16_t memory[MEMORY_MAX]; /* 675536 locations */
+
+uint16_t mem_read(uint16_t addr) {
+	return memory[addr];
+}
--- src/lc3.c
+++ src/lc3.c
@@ -7,9 +7,15 @@
 #include "./hardware/register_store.c"
 
 uint16_t sign_extend(uint16_t x, int bit_count) {
+	// if the left most bit is 1
   if ((x >> (bit_count - 1)) & 1) {
+		// then put all 1 in everything after the bit_count
     x |= (0xFFFF << bit_count);
   }
+	// x = -1 (11111) bit_count = 5
+	// x >> ( 5 - 1 ) = 1
+	// x |= (0xFFFF << 5)
+	// x |= 1111 1111 1110 0000
   return x;
 }
 
@@ -24,11 +30,14 @@ void update_flags(uint16_t r) {
 }
 
 void add(uint16_t instr) {
+	// 0x7 = 111
   uint16_t r0 = (instr >> 9) & 0x7;
   uint16_t r1 = (instr >> 6) & 0x7;
+	// 0x1 = 1
   uint16_t imm_flag = (instr >> 5) & 0x1;
 
   if (imm_flag) {
+		// 0x1f = 11111 , get the last five bits
     uint16_t imm5 = sign_extend(instr & 0x1f, 5);
     reg[r0] = reg[r1] + imm5;
   } else {
@@ -37,6 +46,15 @@ void add(uint16_t instr) {
   }
 
   update_flags(r0);
+}
+
+void ldi(uint16_t instr) {
+	// 0x7 = 111
+	uint16_t dr = (instr >> 9) & 0x7;
+	uint16_t pcoffset9 = sign_extend(instr & 0x1ff, 9);
+
+	reg[dr] = mem_read(mem_read(reg[R_PC] + pcoffset9));
+	update_flags(dr);
 }
 
 int main(int argc, const char *argv[]) {
